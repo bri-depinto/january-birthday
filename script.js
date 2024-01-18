579be290669c9d9196a4ca6c276d32a4e56dbbc7
bri-depinto <144465570+bri-depinto@users.noreply.github.com>
2024-01-18 17:12:58 +0000

    fixed stuff thanks kyle

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function year1894(){
 document.getElementById('changeImage').src = 'images/stanley.jpg'
-document.getElementById('response-text').innerText=' A Stanley Cup is a beautiful looking cup that can keep your drink cold for days… OR it is a trophy awarded to the National Hockey Team CHAMPIONS! On March 22, 1894, the first championship game of the Stanley Cup tournament took place in Montreal, Canada! The Montreal Hockey Club won, defeating the Montreal Victorias 3-2'
+document.getElementById('response-text').innerText=' A Stanley Cup is a beautiful looking cup that can keep your drink cold for days… OR it is a trophy awarded to the National Hockey Team CHAMPIONS! On March 22, 1894, the first championship game of the Stanley Cup tournament took place in Montreal, Canada! The Montreal Hockey Club won, defeating the Montreal Victorias 3-2!'
 document.getElementById('response-date').innerText= 'March 22, 1894'
 }
 function orignalYear(){
 document.getElementById('changeImage').src = 'images/baby.png'
-document.getElementById('response-text').innerText=' idk'
+document.getElementById('response-text').innerText='You see that cute, little baby? Yeah, that is me! The world became a better place on March 22, 2007 because that is when I was born! Nothing else needs to be known about this date except for the fact that it is my birthday! Click on other years below to discover other (less important) events that happened on March 22. To get back here, click on “On my birthday…” at the top! Have fun!'
 document.getElementById('response-date').innerText= 'March 22, 2007'
 }
