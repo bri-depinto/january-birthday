272224d4c106baabe106a1eb6c20b744db3eed0a
bri-depinto <144465570+bri-depinto@users.noreply.github.com>
2024-01-19 00:19:54 +0000

    next button

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ document.getElementById('changeImage').src = 'images/baby.png'
 document.getElementById('response-text').innerText='You see that cute, little baby? Yeah, that is me! The world became a better place on March 22, 2007 because that is when I was born! Nothing else needs to be known about this date except for the fact that it is my birthday! Click on other years below to discover other (less important) events that happened on March 22. To get back here, click on “On my birthday…” at the top! Have fun!'
 document.getElementById('response-date').innerText= 'March 22, 2007'
 }
+function year1957(){
+    document.getElementById('changeImage').src = 'images/dalycity.png'
+    document.getElementById('response-text').innerText='Imagine the whole world shaking beneath your feet? Well for people in San Francisco on March 22, 1957, they did not have to imagine it! A striking earthquake with a magnitude of 5.3 rattled San Francisco which was considered the largest earthquake that struck on the peninsula since 1906! It was highly destructive, taking 3,000 lives and leaving half the city homeless! What a crazy world we live in!'
+    document.getElementById('response-date').innerText= 'March 22, 1957'
+}
