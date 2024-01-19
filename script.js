21930b5f777c7e931757a1bd3fe203546d787118
bri-depinto <144465570+bri-depinto@users.noreply.github.com>
2024-01-19 00:13:38 +0000

    edited picture

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function year1894(){
-document.getElementById('changeImage').src = 'images/stanley.jpg'
+document.getElementById('changeImage').src = 'images/hockey.png'
 document.getElementById('response-text').innerText=' A Stanley Cup is a beautiful looking cup that can keep your drink cold for days… OR it is a trophy awarded to the National Hockey Team CHAMPIONS! On March 22, 1894, the first championship game of the Stanley Cup tournament took place in Montreal, Canada! The Montreal Hockey Club won, defeating the Montreal Victorias 3-2!'
 document.getElementById('response-date').innerText= 'March 22, 1894'
 }
